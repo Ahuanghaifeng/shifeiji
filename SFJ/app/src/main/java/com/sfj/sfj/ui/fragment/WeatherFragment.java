7cafe6433bbdff1db57ab374a0fce36d68f507e8
Ahuanghaifeng <1490738477@qq.com>
2018-05-16 16:16:51 +0800

    天气界面开发，封装组件

--- SFJ/app/src/main/java/com/sfj/sfj/ui/fragment/WeatherFragment.java
+++ SFJ/app/src/main/java/com/sfj/sfj/ui/fragment/WeatherFragment.java
@@ -1,11 +1,16 @@
 package com.sfj.sfj.ui.fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.sfj.sfj.R;
 import com.sfj.sfj.base.BaseDetailFragment;
+import com.sfj.sfj.widget.CommentCardLayout;
+import com.sfj.sfj.widget.EmptyLayout;
 
 public class WeatherFragment extends BaseDetailFragment {
+
+    private CommentCardLayout mWeather;
 
     public static WeatherFragment newInstance() {
 
@@ -21,5 +26,18 @@ public class WeatherFragment extends BaseDetailFragment {
     @Override
     protected int getDetailLayoutId() {
         return R.layout.fragment_weather;
+    }
+
+    @Override
+    protected void initView(View view) {
+        super.initView(view);
+        mSwipeRefreshLayout.setRefreshing(false);
+        mEmptyLayout.setErrorType(EmptyLayout.HIDE_LAYOUT);
+        mWeather = (CommentCardLayout) view.findViewById(R.id.card_weather);
+        mWeather.setTvName("温度");
+        mWeather.setIconHead(R.mipmap.icon_temperature);
+        mWeather.setHorizontalBarChart();
+        //设置数据
+        mWeather.setData(39,"当前温度-摄氏度");
     }
 }
---
+++ SFJ/app/src/main/java/com/sfj/sfj/widget/CommentCardLayout.java
@@ -0,0 +1,114 @@
+package com.sfj.sfj.widget;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.sfj.sfj.R;
+
+import java.util.ArrayList;
+
+public class CommentCardLayout extends RelativeLayout implements OnChartValueSelectedListener{
+
+    private TextView tvName;
+    private HorizontalBarChart mBarChat;
+    private ImageView mHeadImage;
+
+    public CommentCardLayout(Context context) {
+        super(context,null);
+    }
+
+    public CommentCardLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    public void initView(Context context){
+        View view = LayoutInflater.from(context).inflate(R.layout.view_commont_layout,null);
+        tvName = (TextView) view.findViewById(R.id.tv_name);
+        mBarChat = (HorizontalBarChart) view.findViewById(R.id.mHorizontalBarChart);
+        mHeadImage = (ImageView) view.findViewById(R.id.iv_head);
+        addView(view);
+    }
+
+    public void setHorizontalBarChart(){
+        //设置相关属性
+        mBarChat.setOnChartValueSelectedListener(this);
+        mBarChat.setDrawBarShadow(false);
+        mBarChat.setDrawValueAboveBar(true);
+        mBarChat.getDescription().setEnabled(false);
+        mBarChat.setMaxVisibleValueCount(60);
+        mBarChat.setPinchZoom(false);
+        mBarChat.setDrawGridBackground(false);
+
+        //x轴
+        XAxis xl = mBarChat.getXAxis();
+        xl.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xl.setDrawAxisLine(false); //不显示x轴
+        xl.setDrawGridLines(false); //不绘制网格
+        xl.setGranularity(1f); //最小间隔
+        xl.setEnabled(false);
+        mBarChat.getAxisRight().setEnabled(false);
+        mBarChat.setFitBars(true);
+        mBarChat.animateY(2000);
+
+        Legend l = mBarChat.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setFormSize(8f);
+        l.setXEntrySpace(4f);
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
+
+    public void setData(float number,String label){
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        for (int i = 0; i < 1; i++) {
+            entries.add(new BarEntry(0, number));
+        }
+        // 每一个BarDataSet代表一类柱状图
+        BarDataSet barDataSet = new BarDataSet(entries, label);
+        barDataSet.setColor(Color.parseColor("#0a94ff"));
+        barDataSet.setValueTextSize(9f);
+        barDataSet.setFormLineWidth(1f);
+        barDataSet.setFormSize(12.0f);
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(barDataSet);
+        BarData data = new BarData(dataSets);
+        mBarChat.setData(data);
+    }
+
+    public void setTvName(String str){
+        tvName.setText(str);
+    }
+
+    public void setIconHead(int resouce){
+        mHeadImage.setImageResource(resouce);
+    }
+}
